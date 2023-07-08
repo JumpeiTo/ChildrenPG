7034b35f0ed430502b0ef6530362b0e7190d941a
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-07-08 17:19:27 +0000

    [Add]遊び場詳細ページ仮作成

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,11 +5,11 @@ class PlacesController < ApplicationController
     client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
     @keyword = params[:keyword]
     if @keyword.present?
-      type_option
+      query_options = build_query_keyword_options(type_option)
       @places = client.spots_by_query(@keyword, query_options)
     else
-      type_option
-      @places = client.spots_by_query("",query_options)
+      query_options = build_query_keyword_options(type_option)
+      @places = client.spots_by_query("", query_options)
     end
 
     respond_to do |format|
@@ -27,18 +27,27 @@ class PlacesController < ApplicationController
     @radius = params[:radius]
     @lat = params[:lat]
     @lng = params[:lng]
-
-    if @types.present? || @radius.present? || @name.present?
+    
+    if @types.present?
       query_options = build_query_search_options(@types, @radius, @name)
-      @places = client.spots(@lat, @lng, query_options)
-    else
-      @places = []
+    elsif @radius.present? || @name.present?
+      query_options = build_query_search_options(type_option, @radius, @name)
+    else @radius.present? || @name.present?
+      query_options = build_query_search_options(type_option, @radius, nil)
     end
+    @places = client.spots(@lat, @lng, query_options)
 
     respond_to do |format|
       format.html
       format.js
     end
+  end
+  
+  def detailed_search
+    Dotenv.load
+    client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
+    place_id = params[:place_id]
+    @place = client.spot(place_id, :language => 'ja')
   end
 
   private
@@ -50,14 +59,17 @@ class PlacesController < ApplicationController
     options
   end
   
+  # タイプ指定
   def type_option
-    query_options = build_query_keyword_options(['park', 'tourist_attraction', 'amusement_park', 'airport', 'aquarium', 'art_gallery', 'book_store', 'bowling_alley', 'cafe', 'campground', 'department_store', 'hardware_store', 'library', 'movie_theater', 'museum', 'pet_store', 'school', 'restaurant', 'shopping_mall', 'stadium', 'store', ''])
+    ['amusement_park', 'aquarium', 'zoo', 'park', 'tourist_attraction', 'airport', 'art_gallery', 'book_store', 'bowling_alley', 'campground', 'department_store', 'library', 'movie_theater', 'museum', 'pet_store', 'school', 'restaurant', 'shopping_mall', 'stadium', 'store']
   end
 
   # 絞り込み検索用オプション
   def build_query_search_options(types, radius, name)
     options = { language: 'ja' }
-    options[:name] = name if name.present?
+    if name.present?
+      options[:name] = name
+    end
     options[:types] = types if types.present?
     options[:radius] = radius.present? ? radius : 20000
     options

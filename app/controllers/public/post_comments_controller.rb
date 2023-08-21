9a48aefca63d24c1f5b94425e01e51ef368c874a
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-08-21 14:59:11 +0000

    [Update]コメント機能非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,37 +2,20 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_customer!
   def create
     post = Post.find(params[:post_id])
-    comment = current_customer.post_comments.new(post_comment_params)
-    comment.post = post
-    if comment.save
-      flash[:success] = "コメントされました"
+    @comment = current_customer.post_comments.new(post_comment_params)
+    @comment.post_id = post.id
+    if @comment.save
+      # flash[:success] = "コメントされました"
     else
-      flash[:warning] = "コメントを入力してください"
+      # flash[:warning] = "コメントを入力してください"
     end
-    redirect_to request.referer
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    flash[:error] = "コメントが削除されました"
-    redirect_to post_path(params[:post_id])
-  end
-
-  def edit
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    if @comment.update(comment_params)
-      flash[:success] = "コメントを編集しました"
-      redirect_to @post
-    else
-      flash[:danger] = "編集に失敗しました"
-      render "edit"
-    end
+    @comment = PostComment.find(params[:id])
+    post = @comment.post
+    @comment.destroy
+    # flash[:error] = "コメントが削除されました"
   end
 
   private
